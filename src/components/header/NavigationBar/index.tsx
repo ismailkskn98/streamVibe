bd564f80c25ee641da53c254ba64c8bb55c17a61
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-09-11 23:23:59 +0300

    iyileştirmeler gerçekleştirildi.

--- src/components/header/NavigationBar/index.tsx
+++ src/components/header/NavigationBar/index.tsx
@@ -5,10 +5,10 @@ const NavigationBar = () => {
   return (
     <div className="grow hidden lg:flex items-center justify-center">
       <nav className="max-h-[75px] mr-16 xl:mr-36 text-lg flex items-center gap-8 border-4 border-solid border-black-12 bg-black-06 text-grey-75 py-2 px-6 rounded-xl">
-        <NavItem href="/" title="Home" />
-        <NavItem href="/movies" title="Movies & Shows" />
-        <NavItem href="/support" title="Support" />
-        <NavItem href="/subscriptions" title="Subscriptions" />
+        <NavItem href="/" title="Anasayfa" />
+        <NavItem href="/filmler-ve-diziler" title="Filmler ve Diziler" />
+        <NavItem href="/destek" title="Destek" />
+        <NavItem href="/abonelikler" title="Abonelikler" />
       </nav>
     </div>
   );
