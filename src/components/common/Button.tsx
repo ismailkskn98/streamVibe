913ec6c890d3903daa0680cdf27948c6c2f7329b
ismailkskn98 <hamdismailkskn98@gmail.com>
2024-09-16 07:34:12 +0300

    eslint hataları giderildi

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React, { ReactElement } from 'react'
+import React from 'react'
 
 type ButtonProps = {
   Icon?: React.ElementType
--- src/components/faq/FaqAccordion.tsx
+++ src/components/faq/FaqAccordion.tsx
@@ -1,11 +1,6 @@
 'use client';
 import React from 'react'
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import styles from './styles.module.css'
   
 type AccordionItem = {
@@ -63,9 +58,6 @@ const FaqAccordion = () => {
   const midPoint = Math.ceil(accordionData.length / 2);
   const firstColumnFaqs = accordionData.slice(0, midPoint); 
   const secondColumnFaqs = accordionData.slice(midPoint); 
-    console.log(midPoint)
-    console.log(firstColumnFaqs)
-    console.log(secondColumnFaqs)
   return (
     <main className='w-full flex items-start justify-between flex-col xl:flex-row gap-x-5 md:gap-x-10 2xl:gap-x-20'>
         <Accordion type="single" collapsible className='w-full grid grid-cols-1'>
@@ -73,7 +65,7 @@ const FaqAccordion = () => {
               <AccordionItem key={index} value={`item-${index + 1}`} className="border-0">
                 <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
                 <span className=' p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                    {(index + 1) > 9 ? index : `0${index}`}
+                    {(index + 1) > 9 ? (index + 1) : `0${index + 1}`}
                 </span>
                 <div className='w-full space-y-5'>
                   <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>{item.title}</AccordionTrigger>
@@ -85,13 +77,13 @@ const FaqAccordion = () => {
               <div className={`${styles.faqBorder}`}></div>
             </AccordionItem>
           ))}
-          {/* mobil */}
+          {/* Mobil */}
           <div className='w-full block xl:hidden'>
           {secondColumnFaqs.map((item, index)=> (
-              <AccordionItem key={index + midPoint} value={`item-${index + 1}`} className="border-0">
+              <AccordionItem key={index + midPoint + 1} value={`item-${index + midPoint + 1}`} className="border-0">
                 <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
                 <span className=' p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                    {(midPoint + index) > 9 ? (midPoint + index) : `0${midPoint + index}`}
+                    {(midPoint + index + 1) > 9 ? (midPoint + index + 1) : `0${midPoint + index + 1}`}
                 </span>
                 <div className='w-full space-y-5'>
                   <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>{item.title}</AccordionTrigger>
@@ -106,66 +98,22 @@ const FaqAccordion = () => {
           </div>
         </Accordion>
         <Accordion type="single" collapsible className='w-full xl:grid grid-cols-1 hidden'>
-          <AccordionItem value="item-5" className="border-0">
-              <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
-              <span className='p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                  01
-              </span>
-              <div className='w-full space-y-5'>
-                <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>What is the StreamVibe free trial?
-                </AccordionTrigger>
-                <AccordionContent className='text-grey-60 font-light text-sm md:text:base 2xl:text-lg'>
-                  StreamVibe is a streaming service that allows you to watch movies and shows on demand.
-                </AccordionContent>
-              </div>
-              </article>
-            <div className={`${styles.faqBorder}`}></div>
-          </AccordionItem>
-          <AccordionItem value="item-6" className="border-0">
-            <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
-            <span className='p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                01
-            </span>
-            <div className='w-full space-y-5'>
-              <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>How much does StreamVibe cost?
-              </AccordionTrigger>
-              <AccordionContent className='text-grey-60 font-light text-sm md:text:base 2xl:text-lg'>
-                StreamVibe is a streaming service that allows you to watch movies and shows on demand.
-              </AccordionContent>
-            </div>
-            </article>
-          <div className={`${styles.faqBorder}`}></div>
-          </AccordionItem>
-          <AccordionItem value="item-7" className="border-0">
-            <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
-            <span className='p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                01
-            </span>
-            <div className='w-full space-y-5'>
-              <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>What content is available on StreamVibe?
-              </AccordionTrigger>
-              <AccordionContent className='text-grey-60 font-light text-sm md:text:base 2xl:text-lg'>
-                StreamVibe is a streaming service that allows you to watch movies and shows on demand.
-              </AccordionContent>
-            </div>
-            </article>
-          <div className={`${styles.faqBorder}`}></div>
-          </AccordionItem>
-          <AccordionItem value="item-8" className="border-0">
-            <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
-            <span className='p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
-                01
-            </span>
-            <div className='w-full space-y-5'>
-              <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>How do I sign up for StreamVibe?
-              </AccordionTrigger>
-              <AccordionContent className='text-grey-60 font-light text-sm md:text:base 2xl:text-lg'>
-                StreamVibe is a streaming service that allows you to watch movies and shows on demand.
-              </AccordionContent>
-            </div>
-            </article>
-          <div className={`${styles.faqBorder}`}></div>
-          </AccordionItem>
+        {secondColumnFaqs.map((item, index)=> (
+              <AccordionItem key={index + midPoint + 1} value={`item-${index + midPoint + 1}`} className="border-0">
+                <article className='w-full flex items-center jus gap-4 2xl:gap-5 px-[10px] py-5 md:p-6 2xl:p-8'>
+                <span className=' p-3 md:p-4 2xl:p-5 bg-black-12 font-semibold text-xl rounded-xl'>
+                    {(midPoint + index + 1) > 9 ? (midPoint + index + 1) : `0${midPoint + index + 1}`}
+                </span>
+                <div className='w-full space-y-5'>
+                  <AccordionTrigger className='hover:underline-offset-1 text-lg md:text-xl 2xl:text-[22px] font-medium text-white p-0'>{item.title}</AccordionTrigger>
+                  <AccordionContent className='text-grey-60 font-light text-sm md:text:base 2xl:text-lg'>
+                    {item.description}
+                  </AccordionContent>
+                </div>
+                </article>
+              <div className={`${styles.faqBorder}`}></div>
+            </AccordionItem>
+          ))}
         </Accordion>
     </main>
   )
