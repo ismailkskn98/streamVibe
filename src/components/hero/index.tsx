5aa43951f19a7873d6acc92e72b0fd95e3f7dc33
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-09-19 00:07:48 +0300

    hero componenti tasarım iyileştirildi

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,10 +5,10 @@ import Entry from "./entry";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center 2xl:gap-5">
+    <section className="flex flex-col items-center justify-center gap-5 sm:gap-10 lg:gap-20">
       <div className="fluid absolute -top-1 -left-1 -right-1 -bottom-1 w-full h-full bg-black-08 bg-hero-background bg-cover bg-center opacity-5 -z-10"></div>
-      <main className="relative flex items-start justify-center w-full row-start-1 row-end-3 h-[18.75rem] md:h-[32.5rem] 2xl:h-[43rem] text-black-30 z-10 pt-2 md:pt-16">
-        <div className="relative row-start-1 row-end-4 flex items-center justify-center w-[200px] h-[200px] md:w-[300px] md:h-[300px] 2xl:w-[470px] 2xl:h-[470px]">
+      <main className="relative flex items-center justify-center w-full row-start-1 row-end-3 h-[18.75rem] md:h-[32.5rem] 2xl:h-[43rem] text-black-30 z-10 pt-2 md:pt-16">
+        <div className="relative row-start-1 row-end-4 flex items-center justify-center w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[470px] lg:h-[470px]">
           <Image src={desing} alt="logo" fill className="backdrop-blur-[2px]" />
         </div>
       </main>
